11d872f3d26a51f7b5c276318453d7ff04f5d8f9
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-08-10 11:26:47 -0300

    making exceptions and routes

--- users/api.py
+++ users/api.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.hashers import make_password
+from django.core.exceptions import ValidationError
 from .schemas import UserSchema
 from .models import User
 from ninja import Router
@@ -7,9 +8,16 @@ from ninja import Router
 users_router = Router()
 # Django-ninja utiliza schema "exemplo type_user_schema"
 
-@users_router.post('/', response={200: dict})
+@users_router.post('/', response={200: dict, 400: dict, 500: dict})
 def create_user(request, user: UserSchema):
     user = User(**user.dict())
     user.password = make_password(user.password)
-    user.save()
+    try:
+        user.full_clean()
+        user.save()
+    except ValidationError as error:
+        return 400, {'errors': error.message_dict}
+    except Exception as error:
+        return 500, {'errors': 'Erro interno do servidor'}
+
     return {'ok': 'ok'}
