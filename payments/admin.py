d1eedd37b7d4e64c30921aaba285148307c7fd1b
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-08-11 11:09:17 -0300

    init schema to data payment

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Transactions
 
 # Register your models here.
+@admin.register(Transactions)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'payer', 'payee',)
+    list_display_links = ('id', 'payer',)
+    ordering = '-id',
+    search_fields = ('id', 'payer',)
+    list_per_page = 10
+    list_max_show_all = 150
+    list_display_links = 'id', 'payer',
--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from decimal import Decimal
+from users.models import User
 
 # Create your models here.
+class Transactions(models.Model):
+    amount = models.DecimalField(max_digits=15, decimal_places=2, default=Decimal('0.00'), editable=False)
+    payer = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+    payee = models.ForeignKey(User, on_delete=models.CASCADE, editable=False, related_name='payee_user')
+    date = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self) -> str:
+        return f'Saiu de {self.payer.first_name} para {self.payee.first_name} - R${self.amount}'
